c78ae1ba42f7fb9f09d114b3ef59fdf261201e36
Brandon <brandon.jimenez@concordia.ca>
2024-06-17 11:41:23 -0400

    CONUMDLS0303 Cleanup code lib.php
    to tidy up the comparison regarding the loading of the preset

--- lib.php
+++ lib.php
@@ -32,23 +32,18 @@ function theme_saimaniq_get_main_scss_content($theme) {
     $scss = '';
     $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;    
     $fs = get_file_storage();
-
     $context = context_system::instance();
 
-    if ($filename == 'default.scss') {
+    if ($filename == 'default.scss' || $filename == 'plain.scss') {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.    
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss'); 
-    } else if ($filename == 'plain.scss') {
-        // We still load the default preset files directly from the boost theme. No sense in duplicating them.    
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');       
-    } else if ($filename == 'saimaniq.scss') {  
-        $scss .= file_get_contents($CFG->dirroot . '/theme/saimaniq/scss/preset/saimaniq.scss');      
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_saimaniq', 'preset', 0, '/', $filename))) {     
+        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/'.$filename);
+    } else if ($filename == 'saimaniq.scss' || $filename == 'cole.scss') {
+        $scss .= file_get_contents($CFG->dirroot . '/theme/saimaniq/scss/preset/'.$filename);
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_saimaniq', 'preset', 0, '/', $filename))) {
         // This preset file was fetched from the file area for theme_saimaniq and not theme_boost (see the line above).       
         $scss .= $presetfile->get_content();       
     } else {   
-        // Safety fallback - maybe new installs etc.        
-        //$scss .= file_get_contents($CFG->dirroot . '/theme/saimaniq/scss/preset/concordia2.scss');
+        // Safety fallback - maybe new installs etc.
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');    
     }
      // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.    
