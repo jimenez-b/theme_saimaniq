3e52c4afe7a0f25be7f2ab44478877e156ef4eca
Brandon <brandon.jimenez@concordia.ca>
2025-01-17 09:53:20 -0500

    double checker removed
    Fix to remove the check on the modals in the landing page of the quiz

--- amd/src/cole/landing.js
+++ amd/src/cole/landing.js
@@ -2,17 +2,6 @@
 // Code adapted from theme_quizzer
 // converted from jQuery into an ESM module
 
-/**
- * Function to check the status of the element provided
- * @param {object} element element to be tested
- */
-function singleCheck(element) {
-    if (element.checked == true) {
-            document.querySelector('.quizstartbuttondiv button[type="submit"]').disabled = false;
-        } else {
-            document.querySelector('.quizstartbuttondiv button[type="submit"]').disabled = true;
-        }
-}
 
 /**
  * Function to check both
@@ -24,45 +13,6 @@ function bothCheck() {
         } else {
             document.querySelector('.quizstartbuttondiv button[type="submit"]').disabled = true;
         }
-}
-/**
- * Function to check modals
- * @param {object} statusModals status of the modals
- */
-function checkSingle(statusModals) {
-    if (statusModals.copyrightModal == 1 && statusModals.termsModal == 1) {
-        singleCheck(statusModals.where);
-    } else {
-        // eslint-disable-next-line no-alert
-        window.alert('You need to read both the Copyright and the Terms and Conditions');
-        statusModals.where.checked = false;
-    }
-}
-
-/**
- * Function to check modals
- * @param {object} statusModals status of the modals
- */
-function checkSingle2(statusModals) {
-    if (statusModals.copyrightModal != 1 || statusModals.termsModal != 1) {
-        // eslint-disable-next-line no-alert
-        window.alert('You need to read both the Copyright and the Terms and Conditions');
-        statusModals.where.checked = false;
-    }
-}
-
-/**
- * Function to check modals
- * @param {object} statusModals status of the modals
- * @param {object} element element to be tested
- */
-function modalClose(statusModals, element) {
-    if (element.id == 'copyright-modal') {
-        statusModals.copyrightModal = 1;
-    }
-    if (element.id == 'terms-modal') {
-        statusModals.termsModal = 1;
-    }
 }
 
 export const init = () => {
@@ -80,59 +30,15 @@ export const bolder = () => {
 };
 
 export const checkboxEnabler = (instructions, copyright) => {
-    // First, we disable the button
-    let statusModals = {
-        copyrightModal: 0,
-        termsModal: 0,
-        where: ''
-    };
+    //first, we check if both settings are disabled
     if (instructions === "" && copyright === "") {
         //window.console.log('nothing to see here');
         return;
-    }
-    else {
+    } else {
         document.querySelector('.quizstartbuttondiv button[type="submit"]').disabled = true;
-        if (instructions !== "" && copyright !== "") {
-            document.querySelector('input#instructions_verification').addEventListener("click", bothCheck);
-            document.querySelector('input#copyrightnotice').addEventListener("click", bothCheck);
-            // It is to be rendered
-            let selectorCloser = document.querySelectorAll('button.saimaniq-close-modal');
-            selectorCloser.forEach((closerModal) => {
-                closerModal.addEventListener("click", () => {
-                    modalClose(statusModals, closerModal.closest('.saimaniq-modal-box'));
-                });
-            });
-
-            let selector = document.querySelector('input#copyrightnotice');
-            selector.addEventListener("click", () => {
-                statusModals.where = selector;
-                checkSingle2(statusModals);
-            });
-        } else {
-            // We check if instructions is to be rendered by config
-            if (instructions !== "") {
-                // It is to be rendered
-                let selector = document.querySelector('input#instructions_verification');
-                selector.addEventListener("click", () => {
-                    singleCheck(selector);
-                });
-            }
-            if (copyright !== "") {
-                // It is to be rendered
-                let selectorCloser = document.querySelectorAll('button.saimaniq-close-modal');
-                selectorCloser.forEach((closerModal) => {
-                    closerModal.addEventListener("click", () => {
-                        modalClose(statusModals, closerModal.closest('.saimaniq-modal-box'));
-                    });
-                });
-
-                let selector = document.querySelector('input#copyrightnotice');
-                selector.addEventListener("click", () => {
-                    statusModals.where = selector;
-                    checkSingle(statusModals);
-                });
-            }
-        }
+        //now, we check which of the two or both
+        document.querySelector('input#instructions_verification').addEventListener("click", bothCheck);
+        document.querySelector('input#copyrightnotice').addEventListener("click", bothCheck);
     }
 };
 
